d48e3da63665cc31adc4b35d82440bd2177a4dd3
Jason Messler <jason.messler.1@gmail.com>
2021-01-13 21:57:22 -0500

    Add project files.

---
+++ exercise 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            string answer;
+            do
+            {
+                Console.WriteLine("Please enter your text");
+                text = Console.ReadLine();
+                Console.WriteLine(text);
+                Console.WriteLine("Would you like to continue (y/n)?");
+                answer = Console.ReadLine();
+            } while (answer == "y");
+
+            Console.WriteLine("Goodbye");
+           
+
+        }
+    }
+}
